01b65eabc7aa30b71d473413bf2f2058bdb30e98
Miya Li <18900447+miyalee@users.noreply.github.com>
2026-09-24 21:35:27 +1000

    Switch A1A to the eight-ray frame and rebalance the cloud layers

--- A1A_WindowView/sketch.js
+++ A1A_WindowView/sketch.js
@@ -4,15 +4,15 @@ function setup() {
 
 function draw() {
     // Sky color
-    background('#c8e5f0');  
-    
+    background("#c8e5f0");
+
     // Draw clouds
-    randomClouds(14, '#fff');
-    randomClouds(5, '#f9f1f1');
-    randomClouds(1, '#FFF5B4');
+    randomClouds(10, "#fff");
+    randomClouds(5, "#f9f1f1");
+    randomClouds(5, "#FFF5B4");
 
     // Draw window's frame
-    drawFrames(4)
+    drawFrames(8);
 
     noLoop();
 }
@@ -25,12 +25,12 @@ function drawFrames(numSectors) {
 
     for (let i = 0; i < numSectors; i++) {
         // Calculate the angle
-        let angle = i * TWO_PI / numSectors;
+        let angle = (i * TWO_PI) / numSectors;
         // Calculate the end point of the line based on the angle
         let endX = centreX + cos(angle) * len;
         let endY = centreY + sin(angle) * len;
-        
-        stroke('#fff');
+
+        stroke("#fff");
         strokeWeight(10);
         line(centreX, centreY, endX, endY);
     }
@@ -39,24 +39,24 @@ function drawFrames(numSectors) {
 // number: number of clouds to draw
 // color: color of the clouds
 function randomClouds(number, color) {
-   for (let i = 0; i < number; i++) {  
+    for (let i = 0; i < number; i++) {
         // Random position for the cloud
         let x = random(800);
         let y = random(600);
         // Length of the cloud's base line
-        let len = 140; 
+        let len = 140;
 
         stroke(color);
-        fill(color)
+        fill(color);
         strokeWeight(20);
 
         // Base line of the cloud
-        line(x, y, x+len, y); 
+        line(x, y, x + len, y);
 
-        ellipse(x+30, y-10, 25, 10); // Left part of the cloud
-        ellipse(x+70, y-20, 40, 20); // Middle part of the cloud
-        ellipse(x+110, y-10, 25, 10); // Right part of the cloud
-   }
+        ellipse(x + 30, y - 10, 25, 10); // Left part of the cloud
+        ellipse(x + 70, y - 20, 40, 20); // Middle part of the cloud
+        ellipse(x + 110, y - 10, 25, 10); // Right part of the cloud
+    }
 }
 
 // function drawPie(numSectors) {
@@ -64,13 +64,11 @@ function randomClouds(number, color) {
 //     let centreY = 300;
 //     let radius = 1200;
 
-    
 //     let anglePerSector = 360 / numSectors;
 //     for (let i = 0; i < numSectors; i++) {
 //         let startAngle = radians(i * anglePerSector);
 //         let endAngle = radians((i + 1) * anglePerSector);
-        
-  
+
 //         stroke(255);
 //         strokeWeight(10);
 //         fill(138, 195, 206);
